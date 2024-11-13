cdf597ec807f1011db9f6b3d3ad5e29ccaba8bd6
hicarrox1 <hicarrox@gmail.com>
2024-11-13 19:51:22 +0100

    commit 5

---
+++ alumette.py
@@ -0,0 +1,38 @@
+import random
+import toolbox
+
+def afficher_allumettes(allumettes):
+
+    toolbox.afffichage_box(texte = f"Nombre d'allumettes restantes : {allumettes}\n" + "Allumettes : " + "|" * allumettes, padding= 1, center_texte=True)
+
+def jeu_allumettes_2_joueurs():
+    allumettes = 21
+    print("Bienvenue au jeu des allumettes pour 2 joueurs !")
+    print("Il y a 21 allumettes au départ.")
+    print("Vous pouvez retirer entre 1 et 3 allumettes à chaque tour.")
+    print("Le joueur qui prend la dernière allumette perd la partie.\n")
+
+    toolbox.afffichage_box(texte = "Qui commence ? \n'1': Joueur 1\n'2': Joueur 2\n'A': aléatoire", center_texte=True)
+
+    choix = input("").strip()
+    joueur_actuel = int(choix) if choix in [1, 2] else random.choice([1, 2])
+    print(f"C'est le Joueur {joueur_actuel} qui commence.\n")
+
+    while allumettes > 0:
+        afficher_allumettes(allumettes)
+
+        choix_allumettes = int(input(f"Joueur {joueur_actuel}, combien d'allumettes souhaitez-vous retirer ? (1, 2 ou 3) : "))
+
+        if 1 <= choix_allumettes <= 3 and choix_allumettes <= allumettes:
+            allumettes -= choix_allumettes
+        else:
+            print("Choix invalide, essayez à nouveau.")
+            continue
+
+        if allumettes == 0:
+            print(f"Le joueur {joueur_actuel} a pris la dernière allumette. Le joueur {joueur_actuel} a perdu !")
+            break
+
+        joueur_actuel = 2 if joueur_actuel == 1 else 1
+
+jeu_allumettes_2_joueurs()
